18379db7848f215285990f789b8f9af3d9985a9a
Lena <lenajguinot@gmail.com>
2021-10-23 16:40:20 +0900

    added control

--- public/app.js
+++ public/app.js
@@ -2,13 +2,19 @@ document.addEventListener('DOMContentLoaded', () => {
       const grid = document.querySelector('.grid')
       const doodler = document.createElement('div')
       let doodlerLeftSpace = 50
-      let doodlerBottomSpace = 250
+      let startPoint= 150
+      let doodlerBottomSpace = startPoint
       let isGameOver = false
       let platformCount = 5
       let platforms = []
       let upTimerId
       let downTimerId
       let isJumping = true
+      let isGoingLeft = false
+      let isGoingRight = false
+      let leftTimerId
+      let rightTimerId
+
 
       function createDoodler() {
           grid.appendChild(doodler)
@@ -60,7 +66,7 @@ document.addEventListener('DOMContentLoaded', () => {
         upTimerId = setInterval(function() {
           doodlerBottomSpace += 20
           doodler.style.bottom = doodlerBottomSpace + 'px'
-          if (doodlerBottomSpace > 350) {
+          if (doodlerBottomSpace > startPoint + 200) {
             fall()
           }
         },30)
@@ -75,6 +81,19 @@ document.addEventListener('DOMContentLoaded', () => {
           if (doodlerBottomSpace <= 0) {
           gameOver()
           }
+          platforms.forEach(platform => {
+          if (
+              (doodlerBottomSpace >= platform.bottom) && 
+              (doodlerBottomSpace <= platform.bottom + 15) &&
+              ((doodlerLeftSpace + 60) >= platform.left) &&
+              (doodlerLeftSpace <= (platform.left + 85)) &&
+              !isJumping
+            ) {
+                console.log('landed')
+                startPoint = doodlerBottomSpace
+                jump()
+              }
+          })
         },30)
       }
 
@@ -85,14 +104,49 @@ document.addEventListener('DOMContentLoaded', () => {
         clearInterval(downTimerId)
       }
 
-      function control() {
+      function control(e) {
         if (e.key === "ArrowLeft") {
-            //move left
+            moveLeft()
         } else if (e.key === "ArrowRight") {
-            //move right
+            moveRight()
         }  else if (e.key === "ArrowUp") {
-            //move straight
+            moveStraight()
         }
+      }
+
+      function moveLeft() {
+        if (isGoingRight) {
+            clearInterval(rightTimerId)
+            isGoingRight = false
+        }
+        isGoingLeft = true
+        leftTimerId = setInterval(function () {
+            if (doodlerLeftSpace >= 0) { 
+                doodlerLeftSpace -=5
+                doodler.style.left = doodlerLeftSpace + 'px'
+            } else moveRight()
+        },30)
+      }
+
+      function moveRight() {
+        if (isGoingLeft) {
+        clearInterval(leftTimerId)
+        isGoingLeft = false
+        }
+        isGoingRight = true
+        rightTimerId = setInterval(function () {
+            if (doodlerLeftSpace <= 340) { 
+                doodlerLeftSpace +=5
+                doodler.style.left = doodlerLeftSpace + 'px'
+            } else moveLeft()
+        },30)
+      }
+
+      function moveStraight() {
+          isGoingRight = false
+          isGoingLeft = false
+          clearInterval(rightTimerId)
+          clearInterval(leftTimerId)
       }
 
       function start() {
@@ -101,6 +155,7 @@ document.addEventListener('DOMContentLoaded', () => {
           createDoodler()
           setInterval(movePlatforms,30)
           jump()
+          document.addEventListener('keyup',control)
     }
   }
   //attach to button
